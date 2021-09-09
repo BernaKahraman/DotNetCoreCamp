e68f99951120d8a227ee7cf27afac6d33f01cdb3
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-09-09 20:05:08 +0300

    Ders 7

---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Blog
+    {
+        public int BlogID { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogThumbnailImage { get; set; }
+        public string BlogImage { get; set; }
+        public string BlogCreateDate { get; set; }
+        public bool BlogStatus { get; set; }
+
+    }
+
+
+}
