09287cea30cf3275d90192ebdda257836394e3bb
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-09-11 20:42:03 +0300

    Ders 10 - Context Sınıfı

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-S95JB6JC\\SQLEXPRESS01;database=CoreBlogDb; integrated security=true;");
+        }
+    }
+}
