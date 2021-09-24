abcd9a281e6fed989930be4e01e47f26a51114e1
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-09-24 15:05:36 +0300

    update

---
+++ DotNetCoreCamp1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetCoreCamp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
