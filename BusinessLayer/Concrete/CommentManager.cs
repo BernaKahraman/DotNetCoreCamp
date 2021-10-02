649f5678b765fde52a412ecae7cde4fea1a91539
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-10-02 11:59:57 +0300

    Ders 30 - View Component

---
+++ BusinessLayer/Concrete/CommentManager.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CommentManager : ICommentService
+    {
+        ICommentDal _commentdal;
+
+        public CommentManager(ICommentDal commentdal)
+        {
+            _commentdal = commentdal;
+        }
+
+        public void CommentAdd(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comment> GetList(int id)
+        {
+           return _commentdal.GetListAll(x => x.BlogID == id); 
+
+        }
+    }
+}
---
+++ DotNetCoreCamp/ViewComponents/CommentList.cs
@@ -0,0 +1,36 @@
+﻿using DotNetCoreCamp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCamp.ViewComponents
+{
+    public class CommentList: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    Username="Berna"
+
+                },
+                new UserComment
+                {
+                    ID=2,
+                    Username="Eymen"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username="Ebrar"
+                },
+            };
+            return View(commentvalues);
+        }
+    }
+}
