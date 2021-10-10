f96c561e67caae9ce07ff62b488da77fe272ace5
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-10-10 14:24:13 +0300

    Ders 38 - Mail Bülteni

---
+++ BusinessLayer/Concrete/NewsLetterManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class NewsLetterManager : INewsLetterService
+    {
+        INewsLetterDal _newLetterDal;
+
+        public NewsLetterManager(INewsLetterDal newLetterDal)
+        {
+            _newLetterDal = newLetterDal;
+        }
+
+        public void AddNewsLetter(NewsLetter newsLetter)
+        {
+            _newLetterDal.Insert(newsLetter);
+        }
+    }
+}
