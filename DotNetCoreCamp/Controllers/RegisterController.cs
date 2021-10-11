ee40ac636e657908e59717f553726d8c93de65ce
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-10-11 13:52:30 +0300

    Ders 34 - view modelde şehir seçimi

--- DotNetCoreCamp/Controllers/RegisterController.cs
+++ DotNetCoreCamp/Controllers/RegisterController.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +15,12 @@ namespace DotNetCoreCamp.Controllers
     public class RegisterController : Controller
     {
 
-       WriterManager wm = new WriterManager(new EfWriterRepository());
+        WriterManager wm = new WriterManager(new EfWriterRepository());
 
         [HttpGet] //sayfa yüklenince
         public IActionResult Index()
         {
+            ViewBag.Cities = GetCityList();
             return View();
         }
 
@@ -51,9 +53,23 @@ namespace DotNetCoreCamp.Controllers
             }
             return View();
         }
-           
-
-
+        public List<SelectListItem> GetCityList()
+        {
+            List<SelectListItem> adminRole = (from x in GetCity()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x,
+                                                  Value = x
+                                              }).ToList();
+            return adminRole;
         }
+        public List<string> GetCity()
+        {
+            String[] citiesArray = new String[] { "Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Balıkesir", "Bartın", "Batman", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkâri", "Hatay", "Iğdır", "Isparta", "İstanbul", "İzmir", "Kahramanmaraş", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kilis", "Kırıkkale", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Mardin", "Mersin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Şanlıurfa", "Siirt", "Sinop", "Sivas", "Şırnak", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak" };
+            return new List<string>(citiesArray);
+        }
+
+
     }
+}
 
