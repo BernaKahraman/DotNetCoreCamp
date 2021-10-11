a8318ec86a6567bc23048978e2c62e0ae681d1ec
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-10-11 15:54:54 +0300

    Ders 41 - About İşlemleri

---
+++ DotNetCoreCamp/Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCamp.Controllers
+{
+    public class AboutController : Controller
+    {
+
+        AboutManager abm = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult SocialMediaAbout()
+        {
+            var values = abm.GetList();
+            return PartialView(values);
+        }
+    }
+}
