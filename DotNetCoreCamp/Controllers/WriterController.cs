316328094b7fc43f528e7b4ca55e75eb1a3252cb
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-10-11 19:35:49 +0300

    Ders 44 - Authorize

---
+++ DotNetCoreCamp/Controllers/WriterController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCamp.Controllers
+{
+
+    [Authorize]
+    public class WriterController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
+    }
+}
