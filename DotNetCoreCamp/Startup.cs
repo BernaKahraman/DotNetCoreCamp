acd6bbfe0c7f0244a81100e821709e8a4373ab3b
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-10-13 13:42:47 +0300

    Ders 47 - Authorize ile Return Login Url

--- DotNetCoreCamp/Startup.cs
+++ DotNetCoreCamp/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -29,8 +30,6 @@ namespace DotNetCoreCamp
 
             services.AddSession();
 
-
-
             services.AddMvc(config =>
             {
                 var policy = new AuthorizationPolicyBuilder()
@@ -38,7 +37,15 @@ namespace DotNetCoreCamp
                             .Build();
                 config.Filters.Add(new AuthorizeFilter(policy));
             });
-        }
+
+            services.AddMvc();
+            services.AddAuthentication(
+                CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/Login/Index";
+                });
+        }       
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
