6a142f769e61e44c013b1b5166b1625eb02acddc
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-10-24 19:11:27 +0300

    Ders 55 - Yazarın Yeni Blog Eklemesi

--- BusinessLayer/Concrete/BlogManager.cs
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -49,7 +49,7 @@ namespace BusinessLayer.Concrete
 
         public void TAdd(Blog t)
         {
-            throw new NotImplementedException();
+            _blogDal.Insert(t);
         }
 
         public void TDelete(Blog t)
--- DotNetCoreCamp/Controllers/BlogController.cs
+++ DotNetCoreCamp/Controllers/BlogController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -25,6 +28,41 @@ namespace DotNetCoreCamp.Controllers
             return View(values);
         }
 
+        public IActionResult BlogListByWriter()
+        {
+            var values = bm.GetBlogListByWriter(1);
+            return View(values);
+
+        }
+
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BlogAdd(Blog p) //blog sınıfından bir p parametresi 
+        {
+            BlogValidator bv = new BlogValidator();
+            ValidationResult results = bv.Validate(p);
+            if (results.IsValid)
+            {
+                p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                p.WriterID = 1;
+                bm.TAdd(p);
+                return RedirectToAction("BlogListByWriter", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
 
     }
 }
