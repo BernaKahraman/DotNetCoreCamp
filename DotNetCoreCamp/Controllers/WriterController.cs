c0816eaccf0c309e0a6fbfd487e452e3270f4c6c
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-11-08 23:18:55 +0300

    Ders 73 - Profil Fotoğrafını Dosyadan Yükleme

--- DotNetCoreCamp/Controllers/WriterController.cs
+++ DotNetCoreCamp/Controllers/WriterController.cs
@@ -1,12 +1,14 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using DotNetCoreCamp.Models;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -75,6 +77,36 @@ namespace DotNetCoreCamp.Controllers
                 }
             }
             return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterAdd(AddProfileImage p)
+        {
+            Writer w = new Writer();
+            if (p.WriterImage != null)
+            {
+                var extension = Path.GetExtension(p.WriterImage.FileName);
+                var newimagename = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newimagename);
+                var stream = new FileStream(location, FileMode.Create);
+                p.WriterImage.CopyTo(stream);
+                w.WriterImage = newimagename;
+            }
+            w.WriterMail = p.WriterMail;
+            w.WriterName = p.WriterName;
+            w.WriterStatus = true;
+            w.WriterAbout = p.WriterAbout;
+            wm.TAdd(w); 
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }
