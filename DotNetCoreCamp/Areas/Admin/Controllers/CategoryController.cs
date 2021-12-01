f92c9954862f8be8e1c9b3c10f71e7145bf92b56
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-12-01 09:49:52 +0300

    Ders 92 - Pagination

--- DotNetCoreCamp/Areas/Admin/Controllers/CategoryController.cs
+++ DotNetCoreCamp/Areas/Admin/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace DotNetCoreCamp.Areas.Admin.Controllers
 {
@@ -12,9 +13,9 @@ namespace DotNetCoreCamp.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
-            var values = cm.GetList();
+            var values = cm.GetList().ToPagedList(page,3);
             return View(values);
         }
     }
