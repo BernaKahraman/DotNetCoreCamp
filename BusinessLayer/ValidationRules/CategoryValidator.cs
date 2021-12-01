864ea65aa95b440da2c56eca179ae501606f29de
Berna Kahraman <83236586+BernaKahraman@users.noreply.github.com>
2021-12-01 10:19:56 +0300

    Ders 93 - Admin Panelinde Yeni Kategori Ekleme

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator: AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName)
+                .NotEmpty().WithMessage("Kategori adını boş geçemezsiniz!")
+                .MaximumLength(50).WithMessage("Kategori adı en fazla 50 karakter olmalı!")
+                .MinimumLength(2).WithMessage("Kategori adı en az 2 karakter olmalı!");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Kategori açıklamasını boş geçemezsiniz!");
+            
+
+        }
+    }
+}
--- DotNetCoreCamp/Areas/Admin/Controllers/CategoryController.cs
+++ DotNetCoreCamp/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -17,6 +20,33 @@ namespace DotNetCoreCamp.Areas.Admin.Controllers
         {
             var values = cm.GetList().ToPagedList(page,3);
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {   
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(p);
+            if (results.IsValid)
+            {
+                p.CategoryStatus = true;
+                cm.TAdd(p);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
